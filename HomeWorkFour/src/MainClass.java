249a2547f04bb9b463f705c2ab1aa460d80bd52b
KsenyaProk <ksyprok@gmail.com>
2021-01-19 16:17:21 +0300

    Дз4

---
+++ HomeWorkFour/src/MainClass.java
@@ -0,0 +1,69 @@
+public class MainClass {
+
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+
+    public static void main(String[] args) {
+        MainClass w = new MainClass();
+        Thread t1 = new Thread(w::printA);
+        Thread t2 = new Thread(w::printB);
+        Thread t3 = new Thread(w::printC);
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait(100);
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait(100);
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait(100);
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
